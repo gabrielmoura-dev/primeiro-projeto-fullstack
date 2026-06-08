16c121c5fed604d660fe4ba382c61154b16111e1
Gabriel Moura <gabriel-moura22@hotmail.com>
2026-06-08 00:49:59 -0300

    conectando banco de dados PostgreSQL

--- backend-api/index.ts
+++ backend-api/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { handleController } from './src/controllers/helloController.js';
+import pool from './src/database/db.js'
 
 const app = express();
 app.use(cors());
@@ -12,6 +13,11 @@ app.get('/hello-json', (req, res) => {
     const testJson = {"hello": "world"};
     res.json(testJson);
 })
+
+pool.query('SELECT 1')
+    .then(() => console.log('Banco de dados conectado!'))
+    .catch((err) => console.log('Erro ao conectar com o banco:', err));
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
 });
